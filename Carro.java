5e00bcf40fc0e72843b4c471f39362cdd6bb64d9
arisonlucas18 <123038589+arisonlucas18@users.noreply.github.com>
2023-03-03 20:00:28 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Carro {
+    //Variáveis//
+    String Marca, Modelo;
+    int AnoDeFabricacao, VelocidadeAtual;
+    public static void main(String[] args) {
+        //Mensagem//
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A marca do seu carro: ");
+        String Marca = scanner.next();
+
+        System.out.println("Qual o modelo: ");
+        String Modelo = scanner.next();
+
+        System.out.println("O ano de fabricação: ");
+        int AnoDeFabricacao = scanner.nextInt();
+
+        System.out.println("Escreva um programa em Km/h: ");
+        int VelocidadeAtual = scanner.nextInt();
+
+        //Métodos//
+        if (VelocidadeAtual >= 10) {
+            System.out.println("Aumenta a velocidade!");
+        }
+        if (VelocidadeAtual < 10) {
+            System.out.println("Diminuir a velocidade!");
+        }
+        
+        //imprimir informações//
+        System.out.println("Me informe o seu carro " + Marca + ", " + Modelo + ", " + AnoDeFabricacao + " e " + VelocidadeAtual + " Km/h");
+
+        scanner.close();
+    }
+}
